dc2d241d98e90e55543ee6f040b4f92dd0abdf94
ming <zopq4565@gmail.com>
2021-05-06 15:07:16 +0800

    把重複的程式碼改寫成for迴圈且處理沒有館藏時的例外狀況

---
+++ 1book.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.chrome.options import Options
+import unittest, time, re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import math
+import json
+import sys
+
+Link_to_page_URL = sys.argv[1:] 
+if (Link_to_page_URL == ""):
+    print("未輸入網址")
+class NTUNHSLibCrawler(object):
+    def __init__(self,cmdline = None):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless") 
+
+        #----------#
+        #self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver = webdriver.Chrome()
+        #----------#
+        self.driver.implicitly_wait(30)#先等等
+        self.base_url = "https://www.google.com/"#起始URL
+        self.verificationErrors = []
+        self.accept_next_alert = True
+        #我還不懂這兩行能幹嘛
+        
+        """
+        這邊放整個爬蟲流程，去呼叫下面的函數
+        """
+        #爬資料
+        
+        self.parse_bookinfo(Link_to_page_URL[0])
+        #存進去資料庫
+        #
+    def parse_bookinfo(self,Link_to_page_URL):
+        #Keywword = 'python'
+        #filename = Keyword +'.json'
+        driver = self.driver
+        driver.get(Link_to_page_URL)
+        
+        html = driver.page_source
+        #得到當前發好session的網址
+        soup = BeautifulSoup(html, features='html.parser')
+        #丟到解析庫
+
+        #只有一本書的時候
+    
+        bookdata = soup.find('ul', {"class": 'tab_panels pct70 detail_page'})
+        list = [s for s in bookdata.stripped_strings]
+        driver.implicitly_wait(1)
+        try:
+            driver.find_element_by_link_text("預約")
+            print("可預約")
+            driver.implicitly_wait(0)
+        except NoSuchElementException as e:
+            print("無預約")
+        bookinfo = list[1:13]
+        bookstatus = list[19:28]
+        print(bookinfo)
+        print(bookstatus)
+        self.driver.close()#關閉瀏覽器
+        return 0
+    
+            
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        # To know more about the difference between verify and assert,
+        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    c = NTUNHSLibCrawler()
--- lib_crawl_month_now_2021.py
+++ lib_crawl_month_now_2021.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.chrome.options import Options
@@ -21,9 +24,10 @@ class AppDynamicsJob(unittest.TestCase):
         
         chrome_options = Options()
         chrome_options.add_argument("--headless")       # define headless
-        #self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.driver = webdriver.Chrome()
-
+        chrome_options.add_argument('log-level=2') #減少不必要的警告訊息
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        #self.driver = webdriver.Chrome()
+        
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
@@ -40,86 +44,44 @@ class AppDynamicsJob(unittest.TestCase):
         driver.find_element_by_link_text(u"新書-"+str(year)+"年").click()
         driver.find_element_by_link_text(u"新書-"+str(year)+"年 "+str(month)+"月份").click()
         booklist = []
-        driver.find_element_by_link_text(u"新書-"+str(year)+"年 "+str(month)+"月份中文書").click()
-        html = driver.page_source
-        soup = BeautifulSoup(html, features='html.parser')
-        
-        if soup.find('strong').text == "沒有館藏":
-            print(str(year)+"年 "+str(month)+"月沒有館藏")
-        else:
-            searchnum = soup.find('div', {"class": 'searchsummary'})
-            em = searchnum.find_all('em')              # use searchnum as a parent
-            for d in em:
-                print('共{:s}筆資料'.format(d.get_text()))
-            pagenum = math.ceil(int(d.get_text()) / 20)
-            
-            for i in range(pagenum) :
-                print('第{:d}頁'.format(i+1))
+        language_name = ["中文書","西文書","視聽"] #有三種書籍類型
+        for name in language_name: #三種都來一次
+            try : #你先試試看
+                language = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT,u"新書-"+str(year)+"年 "+str(month)+"月份"+name)))
+                #driver在這找值，預設是最多隱性等待10秒
+                language.click()    
+            except NoSuchElementException: 
+                print(str(year)+"年 "+str(month)+"月"+name+"沒有館藏")
+            except TimeoutException: #多半是等到沒耐心了才拋出例外
+                print(str(year)+"年 "+str(month)+"月"+name+"沒有館藏")
+            else :
                 html = driver.page_source
                 soup = BeautifulSoup(html, features='html.parser')
-                hit_list = soup.find_all('li','hit_list_item_info')
-                for list in hit_list:
-                    #print([s for s in list.stripped_strings])#這是有全部資料的
-                    booklist.append([s for s in list.stripped_strings])
-                    #print([s for s in list.stripped_strings][1])#這是只有書名的
-                    #print(list.a.text.strip())
-                if(i == pagenum-1):break
-                if(driver.find_element_by_link_text(">>")):
-                    driver.find_element_by_link_text(">>").click()
-        driver.find_element_by_link_text(u"返回").click()    
-        driver.find_element_by_link_text(u"新書-"+str(year)+"年 "+str(month)+"月份西文書").click()
-        html = driver.page_source
-        soup = BeautifulSoup(html, features='html.parser')
-
-        if soup.find('strong').text == "沒有館藏":
-            print(str(year)+"年 "+str(month)+"月沒有館藏")
-        else:
-            searchnum = soup.find('div', {"class": 'searchsummary'})
-            em = searchnum.find_all('em')              # use searchnum as a parent
-            for d in em:
-                print('共{:s}筆資料'.format(d.get_text()))
-            pagenum = math.ceil(int(d.get_text()) / 20)-1
-            
-            for i in range(pagenum) :
-                print('第{:d}頁'.format(i+1))
-                html = driver.page_source
-                soup = BeautifulSoup(html, features='html.parser')
-                hit_list = soup.find_all('li','hit_list_item_info')
-                for list in hit_list:
-                    #print([s for s in list.stripped_strings])#這是有全部資料的
-                    booklist.append([s for s in list.stripped_strings])
-                    #print([s for s in list.stripped_strings][1])#這是只有書名的
-                    #print(list.a.text.strip())
-                if(i == pagenum-1):break
-                if(driver.find_element_by_link_text(">>")):
-                    driver.find_element_by_link_text(">>").click()
-        driver.find_element_by_link_text(u"返回").click()    
-        driver.find_element_by_link_text(u"新書-"+str(year)+"年"+str(month)+"月份視聽").click()
-        html = driver.page_source
-        soup = BeautifulSoup(html, features='html.parser')
-
-        if soup.find('strong').text == "沒有館藏":
-            print(str(year)+"年 "+str(month)+"月沒有館藏")
-        else:
-            searchnum = soup.find('div', {"class": 'searchsummary'})
-            em = searchnum.find_all('em')              # use searchnum as a parent
-            for d in em:
-                print('共{:s}筆資料'.format(d.get_text()))
-            pagenum = math.ceil(int(d.get_text()) / 20)
-            
-            for i in range(pagenum) :
-                print('第{:d}頁'.format(i+1))
-                html = driver.page_source
-                soup = BeautifulSoup(html, features='html.parser')
-                hit_list = soup.find_all('li','hit_list_item_info')
-                for list in hit_list:
-                    #print([s for s in list.stripped_strings])#這是有全部資料的
-                    booklist.append([s for s in list.stripped_strings])
-                    #print([s for s in list.stripped_strings][1])#這是只有書名的
-                    #print(list.a.text.strip())
-                if(i == pagenum-1):break
-                if(driver.find_element_by_link_text(">>")):
-                    driver.find_element_by_link_text(">>").click()
+                
+                if soup.find('strong').text == "沒有館藏":
+                    print(str(year)+"年 "+str(month)+"月"+name+"沒有館藏")
+                    driver.find_element(By.CSS_SELECTOR, ".button").click()
+                else:
+                    searchnum = soup.find('div', {"class": 'searchsummary'})
+                    em = searchnum.find_all('em')              # use searchnum as a parent
+                    for d in em:
+                        print(name +'共{:s}筆資料'.format(d.get_text()))
+                    pagenum = math.ceil(int(d.get_text()) / 20)
+                    
+                    for i in range(pagenum) :
+                        print('第{:d}頁'.format(i+1))
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, features='html.parser')
+                        hit_list = soup.find_all('li','hit_list_item_info')
+                        for list in hit_list:
+                            #print([s for s in list.stripped_strings])#這是有全部資料的
+                            booklist.append([s for s in list.stripped_strings])
+                            #print([s for s in list.stripped_strings][1])#這是只有書名的
+                            #print(list.a.text.strip())
+                        if(i == pagenum-1):break
+                        if(driver.find_element_by_link_text(">>")):
+                            driver.find_element_by_link_text(">>").click()
+                    driver.find_element_by_link_text(u"返回").click() #點返回回到書籍類型
         
         driver.close() #關閉瀏覽器
         with open(str(year)+str(month)+".json", 'w+', encoding='utf-8') as f:
